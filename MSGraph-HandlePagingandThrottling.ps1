199677f3e38184eee67edb2cfae62c6583efa8ca
Jan Ketil Skanke [MVP] <jan.ketil@outlook.com>
2020-10-01 15:22:00 +0200

    Update MSGraph-HandlePagingandThrottling.ps1

--- MSGraph-HandlePagingandThrottling.ps1
+++ MSGraph-HandlePagingandThrottling.ps1
@@ -1,28 +1,32 @@
-    # Invoke REST method and fetch data until there are no pages left.
-    do {
-        $RetryIn = "0"
-        $ThrottledRun = $false  
-        Write-Output "Querying $Uri..." 
-        try{
-            $Results = Invoke-RestMethod -Method Get -Uri $Uri -ContentType "application/json" -Headers $Header -ErrorAction Continue
+# Scriptmethod created by: Jan Ketil Skanke 
+# Twitter @JankeSkanke
+# https://msendpointmgr.com 
+
+# Invoke REST method and fetch data until there are no pages left.
+do {
+    $RetryIn = "0"
+    $ThrottledRun = $false  
+    Write-Output "Querying $Uri..." 
+    try{
+        $Results = Invoke-RestMethod -Method Get -Uri $Uri -ContentType "application/json" -Headers $Header -ErrorAction Continue
+    }
+    catch{
+        $ErrorMessage = $_.Exception.Message
+        $Myerror = $_.Exception
+        if (($Myerror.Response.StatusCode) -eq "429"){
+            $ThrottledRun = $true
+            $RetryIn = $Myerror.Response.Headers["Retry-After"] 
+            Write-Warning -Message "Graph queries is being throttled"
+            Write-Output "Setting throttle retry to $($RetryIn) seconds"
+        }else
+        {
+            Write-Error -Message "Inital graph query failed with message: $ErrorMessage"
+            Exit 1
         }
-        catch{
-            $ErrorMessage = $_.Exception.Message
-            $Myerror = $_.Exception
-            if (($Myerror.Response.StatusCode) -eq "429"){
-                $ThrottledRun = $true
-                $RetryIn = $Myerror.Response.Headers["Retry-After"] 
-                Write-Warning -Message "Graph queries is being throttled"
-                Write-Output "Setting throttle retry to $($RetryIn) seconds"
-            }else
-            {
-                Write-Error -Message "Inital graph query failed with message: $ErrorMessage"
-                Exit 1
-            }
-        } 
-        if ($ThrottledRun -eq $false){
-            $QueryResults += $Results.value
-        }
-        $uri = $Results.'@odata.nextlink'
-        Start-Sleep -Seconds $RetryIn
-    } until (!($uri))
+    } 
+    if ($ThrottledRun -eq $false){
+        $QueryResults += $Results.value
+    }
+    $uri = $Results.'@odata.nextlink'
+    Start-Sleep -Seconds $RetryIn
+} until (!($uri))
