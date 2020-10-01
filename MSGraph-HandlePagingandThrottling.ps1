6b813566b15482b9797d4201e2de7599bb9f4324
Jan Ketil Skanke [MVP] <jan.ketil@outlook.com>
2020-10-01 14:18:01 +0200

    Create MSGraph-HandlePagingandThrottling.ps1

---
+++ MSGraph-HandlePagingandThrottling.ps1
@@ -0,0 +1,28 @@
+    # Invoke REST method and fetch data until there are no pages left.
+    do {
+        $RetryIn = "0"
+        $ThrottledRun = $false  
+        Write-Output "Querying $Uri..." 
+        try{
+            $Results = Invoke-RestMethod -Method Get -Uri $Uri -ContentType "application/json" -Headers $Header -ErrorAction Continue
+        }
+        catch{
+            $ErrorMessage = $_.Exception.Message
+            $Myerror = $_.Exception
+            if (($Myerror.Response.StatusCode) -eq "429"){
+                $ThrottledRun = $true
+                $RetryIn = $Myerror.Response.Headers["Retry-After"] 
+                Write-Warning -Message "Graph queries is being throttled"
+                Write-Output "Setting throttle retry to $($RetryIn) seconds"
+            }else
+            {
+                Write-Error -Message "Inital graph query failed with message: $ErrorMessage"
+                Exit 1
+            }
+        } 
+        if ($ThrottledRun -eq $false){
+            $QueryResults += $Results.value
+        }
+        $uri = $Results.'@odata.nextlink'
+        Start-Sleep -Seconds $RetryIn
+    } until (!($uri))
